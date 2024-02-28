fa8071523239e1cc40fd7d75c9ee17f3c81fb481
David Martinez <david.antonio.martinez@gmail.com>
2024-02-28 15:13:59 -0800

    fix typo in ProductFile - caused mimeType validation error

--- src/collections/ProductFile.ts
+++ src/collections/ProductFile.ts
@@ -74,7 +74,7 @@ export const ProductFiles: CollectionConfig = {
   upload: {
     staticURL: '/product_files',
     staticDir: 'product_files',
-    mimeTypes: ['image/*,', 'font/*', 'application/postscript']
+    mimeTypes: ['image/*', 'font/*', 'application/postscript']
   },
   fields: [
     {
